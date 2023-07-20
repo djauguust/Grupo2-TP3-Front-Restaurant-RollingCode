92ed840fdbc1d55d4114d19571480f9dd3cca77e
Yudi454 <lucasyudi445@gmail.com>
2023-07-19 23:14:01 -0300

    Le agregue una frase en base a la tematica de nuestro Restaurante, le agregue un fondo y deje responsive

--- src/Pages/Error404/Error404.jsx
+++ src/Pages/Error404/Error404.jsx
@@ -5,7 +5,20 @@ import { Button } from 'react-bootstrap';
 const Error404 = () => {
   return (
     <>
-     <h1>Erro404</h1>
+     <div className='Imagen-Fondo'>
+        <div className='Fondo-Oscuro'>
+          <div className='Cuerpo-Error'>
+          <div className='text-center mb-4'>
+          <h1 className='mb-3'>Lost in the Pasta</h1>
+          <h3>¡Oops! Parece que te has perdido en nuestra deliciosa pasta. 
+	            siempre puedes volver a la página de inicio para encontrar el camino de regreso a la auténtica experiencia italiana que ofrecemos.</h3>
+          </div>
+          <div className='d-flex justify-content-center '>
+          <Button className='btn-Volver'>Volver al Inicio</Button>
+          </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
