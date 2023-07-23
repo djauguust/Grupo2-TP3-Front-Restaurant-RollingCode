8f5abed7d14cf758ab6ce01cc3edd4414b1a7bb2
Yudi454 <lucasyudi445@gmail.com>
2023-07-23 13:08:52 -0300

    Cree un formulario para cambiar los datos del usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import Rutes from './routes/Rutes'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
 
 function App() {
 
 
   return (
     <>
+    <Navbar />
       <main>
         <Rutes />
       </main>
+    <Footer />
     </>
   )
 }
--- src/pages/Configurar-Cuenta/configurar-cuenta.jsx
+++ src/pages/Configurar-Cuenta/configurar-cuenta.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Button, Container, Form, Stack } from 'react-bootstrap'
+import "../../style/configurar-cuenta.css"
 
 const configurarCuenta = () => {
   return (
     <>
-    <h1>configurar-cuenta</h1>
+    <div className='Contenedor-Cuerpo'>
+    <div className='text-center'>
+        <h1>Cambia los datos de tu cuenta :</h1>
+    </div>
+    <h3></h3>
+    <Container>
+        <div className='Contenedor-Form'>    
+        <Form>
+        <Stack gap={2}>
+            <Form.Group>
+                <Form.Label>Nombre :</Form.Label>
+                <Form.Control />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Apellido :</Form.Label>
+                <Form.Control />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Email :</Form.Label>
+                <Form.Control />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Contraseña   :</Form.Label>
+                <Form.Control />
+            </Form.Group>
+        </Stack>
+            <Button className='btn-Volver mt-3'>Volver al Inicio</Button>
+        </Form>
+        </div>
+    </Container>
+    </div>
     </>
   )
 }
