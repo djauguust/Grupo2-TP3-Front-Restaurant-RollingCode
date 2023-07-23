659417f48ca7e863e327027dd01b8e0835550402
Yudi454 <lucasyudi445@gmail.com>
2023-07-23 15:00:34 -0300

    Cree un esquema para el formulario de configurar cuenta

--- src/pages/Configurar-Cuenta/configurar-cuenta.jsx
+++ src/pages/Configurar-Cuenta/configurar-cuenta.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 import { Button, Container, Form, Stack } from 'react-bootstrap'
 import "../../style/configurar-cuenta.css"
+import {useFormik} from "formik";
+import * as Yup from "yup" ;
+import clsx from "clsx";
 
 const configurarCuenta = () => {
+
+    const soloLetras= /^[a-zA-Z ]+$/
+    const email = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+    const contraseña= /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/
+
+    const esquemaConfigurarCuenta = Yup.object().shape({
+        Nombre: Yup.string()
+        .required("El Nombre es Requerido")
+        .matches(soloLetras, "Solo son validas letras")
+        .min(4,"Ingrese un nombre mayor de 4 letras")
+        .max(25,"Ingrese menos de 25 letras"),
+
+        Apellido: Yup.string()
+        .required("El Apellido es Requerido")
+        .matches(soloLetras, "Solo son validas letras")
+        .min(4,"Ingrese mas de 4 letras")
+        .max(25,"Ingrese menos de 25 letras"),
+
+        Email: Yup.string()
+        .required("El email es requerido")
+        .matches(email,"Ingrese un formato de email correcto")
+        .min(16,"Ingrese un email mayor a 16 carácteres")
+        .max(40,"Ingrese un email menor a 40 carácteres"),
+
+        Contraseña: Yup.string()
+        .required("La contraseña es requerida")
+        .matches(contraseña,"La contraseña debe de contener entre 8 y 16 carácteres, al menos un dígito, al menos una minuscula y al menos una mayuscula"),
+
+        RepetirContraseña:Yup.string()
+        .required("Debe ingresar la contraseña nuevamente")
+        .oneOf([Yup.ref('Contraseña')],'Las contraseñas deben de coincidir')
+    })
+
   return (
     <>
     <div className='Contenedor-Cuerpo'>
