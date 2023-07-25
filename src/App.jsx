231570050a1068d77358314db0e243fb6f158a70
PedroAntich <pedroantichi@gmail.com>
2023-07-25 14:58:13 -0300

    Primer maqueta pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+
+import CarruselPrincipal from './components/CarruselPrincipal';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
-
-
   return (
     <>
-      
+      <Header />
+      <CarruselPrincipal />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
