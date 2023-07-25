0f00ba1f8ac640d409d758af24b0db0b92577371
Mauro <mauro10z@hotmail.com>
2023-07-24 23:55:08 -0300

    Creacion pagina A cerca de Nosotros

---
+++ src/pages/a-cerca-de-nosotros/ACercaDeNosotros.jsx
@@ -0,0 +1,216 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+
+const ACercaDeNosotros = () => {
+  return (
+    <>
+      <h1>A Cerca De Nosotros</h1>
+      <p>Somos un grupo de programadores que...</p>
+
+      <Container>
+        <Row>
+          <Col>
+            <Card 
+              style={{ 
+                width: "18rem",
+                marginTop: "3rem"
+                }} className="about-card">
+              <Image
+                className="about-img"
+                src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
+                roundedCircle
+              />
+              <Card.Body>
+                <Card.Title>Nombre</Card.Title>
+                <Card.Text>
+                  Descripcion breve. Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Dicta non voluptate dolore dolores unde
+                  similique sequi assumenda ducimus laudantium enim.
+                </Card.Text>
+                <div>
+                  <a href="">
+                    <i class="bi bi-github m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-linkedin m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-instagram m-2"></i>
+                  </a>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card 
+              style={{ 
+                width: "18rem",
+                marginTop: "3rem"
+                }} className="about-card">
+              <Image
+                className="about-img"
+                src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
+                roundedCircle
+              />
+              <Card.Body>
+                <Card.Title>Nombre</Card.Title>
+                <Card.Text>
+                  Descripcion breve. Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Dicta non voluptate dolore dolores unde
+                  similique sequi assumenda ducimus laudantium enim.
+                </Card.Text>
+                <div>
+                  <a href="">
+                    <i class="bi bi-github m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-linkedin m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-instagram m-2"></i>
+                  </a>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card 
+              style={{ 
+                width: "18rem",
+                marginTop: "3rem"
+                }} className="about-card">
+              <Image
+                className="about-img"
+                src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
+                roundedCircle
+              />
+              <Card.Body>
+                <Card.Title>Nombre</Card.Title>
+                <Card.Text>
+                  Descripcion breve. Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Dicta non voluptate dolore dolores unde
+                  similique sequi assumenda ducimus laudantium enim.
+                </Card.Text>
+                <div>
+                  <a href="">
+                    <i class="bi bi-github m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-linkedin m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-instagram m-2"></i>
+                  </a>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+        <Col>
+            <Card 
+              style={{ 
+                width: "18rem",
+                marginTop: "3rem"
+                }} className="about-card">
+              <Image
+                className="about-img"
+                src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
+                roundedCircle
+              />
+              <Card.Body>
+                <Card.Title>Nombre</Card.Title>
+                <Card.Text>
+                  Descripcion breve. Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Dicta non voluptate dolore dolores unde
+                  similique sequi assumenda ducimus laudantium enim.
+                </Card.Text>
+                <div>
+                  <a href="">
+                    <i class="bi bi-github m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-linkedin m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-instagram m-2"></i>
+                  </a>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card 
+              style={{ 
+                width: "18rem",
+                marginTop: "3rem"
+                }} className="about-card">
+              <Image
+                className="about-img"
+                src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
+                roundedCircle
+              />
+              <Card.Body>
+                <Card.Title>Nombre</Card.Title>
+                <Card.Text>
+                  Descripcion breve. Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Dicta non voluptate dolore dolores unde
+                  similique sequi assumenda ducimus laudantium enim.
+                </Card.Text>
+                <div>
+                  <a href="">
+                    <i class="bi bi-github m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-linkedin m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-instagram m-2"></i>
+                  </a>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card 
+              style={{ 
+                width: "18rem",
+                marginTop: "3rem"
+                }} className="about-card">
+              <Image
+                className="about-img"
+                src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
+                roundedCircle
+              />
+              <Card.Body>
+                <Card.Title>Nombre</Card.Title>
+                <Card.Text>
+                  Descripcion breve. Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Dicta non voluptate dolore dolores unde
+                  similique sequi assumenda ducimus laudantium enim.
+                </Card.Text>
+                <div>
+                  <a href="">
+                    <i class="bi bi-github m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-linkedin m-2"></i>
+                  </a>
+                  <a href="">
+                    <i class="bi bi-instagram m-2"></i>
+                  </a>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default ACercaDeNosotros;
