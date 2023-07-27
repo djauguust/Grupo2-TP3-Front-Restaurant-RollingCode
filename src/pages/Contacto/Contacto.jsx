f9cbc5709b38cb098a8fa833e195ea5bf99859fd
Lucas Ybañez <luck.facebook@hotmail.com>
2023-07-27 00:30:21 -0300

    contacto: mapa

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -1,12 +1,31 @@
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
+import Mapa from "../../components/Mapa"
+import style from "./Contacto.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Contacto = () =>{
 
     return(
         <>
-            <Header/>
-            <Footer/> 
+            <div className={style.container}>
+
+                <div className={style.header}>
+                    <Header/>
+                </div>
+
+                <h5 className={`m-5 text-center ${style.parrafo}`}>
+                    ¡Tu satisfacción es nuestra prioridad! Si tienes alguna pregunta, inquietud o necesitas asistencia para hacer una reserva, no dudes en contactarnos. Nuestro equipo estará encantado de ayudarte en todo lo que necesites para que disfrutes de una experiencia inolvidable en nuestro restaurante.
+                </h5>
+
+                <div className={`m-5 p-5 ${style.mapa}`}>
+                    <Mapa></Mapa>
+                </div>
+
+                <div className={style.footer}>
+                    <Footer/>
+                </div>
+            </div>
         </>
     )
 }
