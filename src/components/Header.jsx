d468e6c4201bd9de1d62e0d8ef0ddfa5c6b735fe
PedroAntich <pedroantichi@gmail.com>
2023-07-27 16:00:04 -0300

    efecto hover botones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import "./Header.css"
 
 const Header = () => {
   return (
@@ -9,7 +10,7 @@ const Header = () => {
   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
   <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
 </svg> Iniciar Sesion | Ubicacion | Idioma</div>
-      <Navbar expand="lg" className="">
+      <Navbar expand="lg" className="custom-navbar">
           <Navbar.Brand href="#home" className='ms-4'>
             <img
               alt=""
@@ -22,11 +23,11 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className='justify-content-center'>
               <Nav className="ml-auto mb-2 mb-lg-0">
-                <Nav.Link href="#home" className='text-dark m-2'>INICIO</Nav.Link>
-                <Nav.Link href="#link" className='text-dark m-2'>QUIENES SOMOS</Nav.Link>
-                <Nav.Link href="#link" className='text-dark m-2'>GALERIA</Nav.Link>
-                <Nav.Link href="#link" className='text-dark m-2'>CONTACTO</Nav.Link>
-                <Nav.Link href="#link" className="bg-black text-light m-2">
+                <Nav.Link href="#home" className='text-dark m-2 botones'>INICIO</Nav.Link>
+                <Nav.Link href="#link" className='text-dark m-2 botones'>QUIENES SOMOS</Nav.Link>
+                <Nav.Link href="#link" className='text-dark m-2 botones'>GALERIA</Nav.Link>
+                <Nav.Link href="#link" className='text-dark m-2 botones'>CONTACTO</Nav.Link>
+                <Nav.Link href="#link" className=" text-light m-2 boton-reserva">
                   HAZ TU RESERVA
                 </Nav.Link>
                 {/* NavDropdown... */}
