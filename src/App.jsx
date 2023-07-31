6d82252e53aaa446bec96065ce3fa24785964330
Mauro <mauro10z@hotmail.com>
2023-07-31 19:06:20 -0300

    Creacion estructura de componente reserva

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import './App.css'
+import Reservas from './components/pages/reservas/Reservas'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Reservas</h1>
-        <button>Registrate</button>
-     </div>
-
-     <div>
-      <h3>1.Encuentra una mesa</h3>
-      <h3>2.Detalles</h3>
-     </div>
-
-     
+      <Reservas/>
     </>
   )
 }
--- src/components/pages/reservas/Reservas.jsx
+++ src/components/pages/reservas/Reservas.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function Reservas() {
   return (
-    <div>Reservas</div>
+    <>  
+      <main style={{
+         width:'100%',
+         height:'400px',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+      }}>
+        <Container>
+          <Row>
+            <Col>
+              <Image src="holder.js/171x180" thumbnail />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <h1>Reserva tu mesa</h1>
+            </Col>
+            <Col>
+              <button>Registrate!</button>
+            </Col>
+          </Row>
+
+          <Row>
+            <h2>Calendar</h2>
+          </Row>
+        </Container>
+      </main>
+      {/* <main style={{
+        width:'100%',
+        height:'400px',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+      }}>
+
+        <div className="container" style={{display:'flex', flexDirection:'column'}}>
+
+          <div className="row" style={{display:'flex'}}>
+            <div className="col">
+              <Image src="holder.js/171x180" thumbnail />
+            </div>
+
+          </div>
+
+          <div className="row" style={{display:'flex'}}>
+            <div className="col">
+              <h1>Reserva tu mesa</h1>
+            </div>
+            <div className="col">
+              <button>Registrate!</button>
+            </div>
+          </div>  
+            
+            <div className="row" style={{display:'flex'}}>
+              <h2>Calendar</h2>
+            </div>
+          
+        </div>
+      </main> */}
+
+    </>
   )
 }
 
