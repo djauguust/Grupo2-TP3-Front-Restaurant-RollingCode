247c6aad253a0f457c8822e23658ef5321209af4
PedroAntich <pedroantichi@gmail.com>
2023-08-03 11:08:13 -0300

    agrego imagenes a galeria

--- src/components/galeria/Galeria.jsx
+++ src/components/galeria/Galeria.jsx
@@ -7,14 +7,12 @@ const Galeria = () => {
       <body className="body-galeria">
       <h2 className="text-center text-light">Galeria</h2>
       <div className="grid-container">
-        <div className="grid-item wide img-1" ></div>
-        <div className="grid-item img-2"></div>
-        <div className="grid-item tall img-3">3</div>
-        <div className="grid-item img-4">4</div>
-        <div className="grid-item img-5">5</div>
-        <div className="grid-item wide img-6">6</div>
-        <div className="grid-item tall img-7">7</div>
-        <div className="grid-item img-8">8</div>
+        <div className="grid-item img-1" ><img src="public\galeria\restaurant-terrazza-danieli-at-dusk.jpg" alt="" className="img-1"/></div>
+        <div className="grid-item img-2"><img src="public\galeria\pizza.jpg" alt="" className="img-1"/></div>
+        <div className="grid-item tall img-3"><img src="public\galeria\huge.jpg" alt="" className="img-1"/></div>
+        <div className="grid-item img-4"><img src="public\galeria\cena.jpg" alt="" className="img-1"/></div>
+        <div className="grid-item img-5 wide"><img src="public\galeria\Dabble_Entrance.jpg" alt="" className="img-2"/></div>
+        <div className="grid-item img-6"><img src="public\galeria\FQunKDyKt_720x0__1.jpg" alt="" className="img-1"/></div>
       </div>
       </body>
     </>
