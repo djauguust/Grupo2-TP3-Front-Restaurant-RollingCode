a327c0f64859fb40d180cf76238a230d3ed1e470
Lucas Ybañez <luck.facebook@hotmail.com>
2023-08-06 18:02:54 -0300

    Admin: Header y footer

--- src/pages/Administrador/Administrador.jsx
+++ src/pages/Administrador/Administrador.jsx
@@ -1,8 +1,13 @@
+import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
+
 const Admin = () =>{
 
     return(
         <>
+            <Header/>
             <h1>Hola mundo</h1>
+            <Footer/>
         </>
     )
 }
