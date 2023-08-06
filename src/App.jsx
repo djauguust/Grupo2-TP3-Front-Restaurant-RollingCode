529aea1b5066bbc9d72806976edcc91f6f08b153
Lucas Ybañez <luck.facebook@hotmail.com>
2023-08-06 17:55:24 -0300

    admin: creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Admin from './pages/Administrador/Administrador'
 
 function App() {
 
 
   return (
     <>
-      
+      <Admin/>
     </>
   )
 }
---
+++ src/pages/Administrador/Administrador.jsx
@@ -0,0 +1,10 @@
+const Admin = () =>{
+
+    return(
+        <>
+            <h1>Hola mundo</h1>
+        </>
+    )
+}
+
+export default Admin;
