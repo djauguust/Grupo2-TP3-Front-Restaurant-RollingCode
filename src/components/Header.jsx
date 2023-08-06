ff4e60c0f0f53292d085815844dd65b7851f1c34
Lucas Ybañez <luck.facebook@hotmail.com>
2023-08-06 17:58:23 -0300

    contacto: nuevo header y footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import "./Footer.css"
-
 
 const Header = () => {
   return (
@@ -11,7 +9,7 @@ const Header = () => {
   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
   <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
 </svg> Iniciar Sesion | Ubicacion | Idioma</div>
-      <Navbar expand="lg" className="footer">
+      <Navbar expand="lg" className="">
           <Navbar.Brand href="#home" className='ms-4'>
             <img
               alt=""
