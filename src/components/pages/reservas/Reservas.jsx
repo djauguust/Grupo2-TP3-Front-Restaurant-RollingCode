513954ab1c66091b485db4547e7f848e4daf63af
Mauro <mauro10z@hotmail.com>
2023-08-08 21:36:14 -0300

    Modifico input time, el componente de datePicker y su funcionamiento

--- src/components/pages/reservas/Reservas.jsx
+++ src/components/pages/reservas/Reservas.jsx
@@ -6,94 +6,116 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const Reservas = () => {
+  // Calendario
   let day = new Date();
   const [startDate, setStartDate] = useState(day);
   const handleDate = (date) => {
     setStartDate(date);
   };
 
+  //Hora
+  const [startTime, setStartTime] = useState(day);
+  const handleTime = (time) => {
+    setStartTime(time);
+  }
+  console.log(startTime);
+    const convertToNumericTime = (time) => {
+      const hours = time.getHours();
+      const minutes = time.getMinutes();
+      return hours * 100 + minutes;
+    };
+    const filterTime = (time) => {
+      const hours = new Date(time).getHours();
+      return hours < 0 || hours > 6;
+    };
 
-  const initialValues = {
-    time: "",
-    date: day,
-    people: "",
+    //Formik
+    const initialValues = {
+      time: convertToNumericTime(startTime),
+      date: startDate,
+      people: "",
+    };
+
+    const sendForm = (values) => {
+      console.log("Hora:", convertToNumericTime(startTime));
+      console.log("Fecha:", startDate);
+      console.log("Cantidad de personas:", values.people);
+    };
+
+    const { handleChange, handleSubmit, values } = useFormik({
+      initialValues,
+
+      validationSchema: Yup.object({
+        time: Yup.number().required("La hora es requerida"),
+        date: Yup.date().required("La fecha es requerida"),
+        people: Yup.number()
+          .typeError("La cantidad de personas debe ser un número")
+          .required("La cantidad de personas es requerida")
+          .positive("La cantidad de personas debe ser mayor a cero"),
+      }),
+
+      onSubmit: sendForm,
+    });
+
+    return (
+      <>
+        <main
+          style={{
+            height: "500px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Container>
+            <Row>
+              <Col>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group controlId="time">
+                    <Form.Label>Hora</Form.Label>
+                    <DatePicker
+                      selected={startTime}
+                      onChange={handleTime}
+                      showTimeSelect
+                      showTimeSelectOnly
+                      timeIntervals={30}
+                      timeCaption="Time"
+                      dateFormat="HH:mm"
+                      filterTime={filterTime}
+                      value={values.time}
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="date">
+                    <Form.Label>Fecha</Form.Label>
+                    <DatePicker
+                      selected={startDate}
+                      onChange={handleDate}
+                      minDate={day}
+                      dateFormat="dd/MM/yyyy"
+                      filterDate={(date) => date.getDay() !== 1}
+                      value={values.date}
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="people">
+                    <Form.Label>Cantidad de personas</Form.Label>
+                    <Form.Control
+                      type="number"
+                      onChange={handleChange}
+                      value={values.people}
+                    />
+                  </Form.Group>
+
+                  <Button variant="primary" type="submit">
+                    Enviar
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
+        </main>
+      </>
+    );
   };
-
-  // const validationSchema = Yup.object().shape({
-  //   time: Yup.string().required("La hora es requerida"),
-  //   date: Yup.date().required("La fecha es requerida"),
-  //   people: Yup.number()
-  //     .typeError("La cantidad de personas debe ser un número")
-  //     .required("La cantidad de personas es requerida")
-  //     .positive("La cantidad de personas debe ser mayor a cero"),
-  // });
-
-  const handleSubmit = (values) => {
-    // Aquí puedes realizar alguna acción con los datos del formulario
-    console.log("Hora:", values.time);
-    console.log("Fecha:", startDate);
-    console.log("Cantidad de personas:", values.people);
-  };
-
-  const formik = useFormik({
-    initialValues: { initialValues },
-    // validationSchema: { validationSchema },
-    onSubmit: handleSubmit,
-  });
-
-  return (
-    <>
-      <main
-        style={{
-          height: "500px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Container>
-          <Row>
-            <Col>
-              <Form onSubmit={formik.handleSubmit}>
-                <Form.Group controlId="time">
-                  <Form.Label>Hora</Form.Label>
-                  <Form.Control
-                    type="time"
-                    onChange={formik.handleChange}
-                    min="00:00"
-                    max="06:00"
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="date">
-                  <Form.Label>Fecha</Form.Label>
-                  <DatePicker
-                    selected={startDate}
-                    onChange={handleDate}
-                    minDate={day}
-                    dateFormat='dd/MM/yyyy'
-                    filterDate={date=>date.getDay()!==1}
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="people">
-                  <Form.Label>Cantidad de personas</Form.Label>
-                  <Form.Control
-                    type="number"
-                    onChange={formik.handleChange}
-                  />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                  Enviar
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-      </main>
-    </>
-  );
-};
-
 export default Reservas;
