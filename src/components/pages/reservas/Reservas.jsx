c801f59842f7977dcffbb869a0f8318b55eda23b
Mauro <mauro10z@hotmail.com>
2023-08-10 17:30:22 -0300

    Modificando estilos de css

--- src/components/pages/reservas/Reservas.jsx
+++ src/components/pages/reservas/Reservas.jsx
@@ -6,6 +6,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./reserva.css";
+import Image from "react-bootstrap/Image";
 
 const Reservas = () => {
   // Calendario
@@ -75,76 +76,88 @@ const Reservas = () => {
     <>
       <main className="reservation-main">
         <Container className="reservation-container">
-          <Row>
-            <Form
-              onSubmit={handleSubmit}
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height:'400px'
-              }}
-            >
-              <Col md={4}>
-                <Form.Group controlId="people">
-                  <Form.Control
-                    className="inputReservation"
-                    placeholder="N° de Personas"
-                    type="number"
-                    onChange={handleChange}
-                    value={values.people}
-                    required
-                    min={1}
-                    max={10}
-                  />
-                </Form.Group>
+          <div className="small-box">
+            <Row>
+            <Col xs={6} md={4}>
+                <Image
+                  style={{width:'60xp',height:'60px'}}
+                  src="https://trello.com/1/cards/64b73c636625809102489870/attachments/64c047b08259f586953a21f3/download/logo_nuevo.png"
+                  rounded
+                />
               </Col>
+            </Row>
 
-              <Col md={4}>
-                <Form.Group controlId="date">
-                  <DatePicker
-                    className="inputReservation"
-                    selected={startDate}
-                    onChange={handleDate}
-                    minDate={filterMinDay()}
-                    maxDate={filterMaxDay()}
-                    dateFormat="dd/MM/yyyy"
-                    filterDate={(date) => date.getDay() !== 1}
-                    value={values.date}
-                    required
-                  />
-                </Form.Group>
-              </Col>
+            <Row>
+              
+              <Form
+                onSubmit={handleSubmit}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100px",
+                }}
+              >
+                <Col md={4}>
+                  <Form.Group controlId="people">
+                    <Form.Control
+                      className="inputReservation"
+                      placeholder="N° de Personas"
+                      type="number"
+                      onChange={handleChange}
+                      value={values.people}
+                      required
+                      min={1}
+                      max={10}
+                    />
+                  </Form.Group>
+                </Col>
 
-              <Col md={4}>
-                <Form.Group controlId="time">
-                  <DatePicker
-                    className="inputReservation"
-                    selected={startTime}
-                    onChange={handleTime}
-                    showTimeSelect
-                    showTimeSelectOnly
-                    timeIntervals={30}
-                    timeCaption="Time"
-                    dateFormat="HH:mm"
-                    filterTime={filterTime}
-                    value={values.time}
-                    required
-                  />
-                </Form.Group>
-              </Col>
-              <Col>
-                <Button
-                  variant="primary"
-                  type="submit"
-                  className="inputReservation m-3"
-                  
-                >
-                  Reserva
-                </Button>
-              </Col>
-            </Form>
-          </Row>
+                <Col md={4}>
+                  <Form.Group controlId="date">
+                    <DatePicker
+                      className="inputReservation"
+                      selected={startDate}
+                      onChange={handleDate}
+                      minDate={filterMinDay()}
+                      maxDate={filterMaxDay()}
+                      dateFormat="dd/MM/yyyy"
+                      filterDate={(date) => date.getDay() !== 1}
+                      value={values.date}
+                      required
+                    />
+                  </Form.Group>
+                </Col>
+
+                <Col md={4}>
+                  <Form.Group controlId="time">
+                    <DatePicker
+                      className="inputReservation"
+                      selected={startTime}
+                      onChange={handleTime}
+                      showTimeSelect
+                      showTimeSelectOnly
+                      timeIntervals={30}
+                      timeCaption="Time"
+                      dateFormat="HH:mm"
+                      filterTime={filterTime}
+                      value={values.time}
+                      required
+                    />
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    className="inputReservation m-3"
+                  >
+                    Reserva
+                  </Button>
+                </Col>
+              </Form>
+            </Row>
+          </div>
         </Container>
       </main>
     </>
