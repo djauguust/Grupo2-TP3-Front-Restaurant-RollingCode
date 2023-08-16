f31205833ee0f9c621674b923032a807da8a513b
Yudi454 <lucasyudi445@gmail.com>
2023-08-15 23:32:55 -0300

    Modifique el los input de hora y fecha en crear reserva para probar como quedaban con las validaciones, proximamente los estare pasando al modal

--- src/paginas/crearReservasPrueba/crearReservasPrueba.jsx
+++ src/paginas/crearReservasPrueba/crearReservasPrueba.jsx
@@ -5,19 +5,26 @@ import * as Yup from "yup";
 import es from "date-fns/locale/es";
 import "react-datepicker/dist/react-datepicker.css";
 import { Form, Stack, Button } from "react-bootstrap";
-import { format } from "date-fns";
+import { format, getDay } from "date-fns";
+import { subDays } from "date-fns";
 
 const crearReservasPrueba = () => {
 //Pagina de prueba para crear reservas para saber como funciona datepicker y para saber como llegan los datos
 
+let date = new Date();
+
 //Esquema de con .date para que guarde la fecha
   const esquema = Yup.object().shape({
     FechaReserva: Yup.date().required("Fecha es requerida"),
+
+    HoraReserva: Yup.string()
+    .required("La hora es requerida")
   });
 
   //Valores null porque no se puede tener vacio
   const valoresIniciales = {
     FechaReserva: null,
+    HoraReserva: ""
   };
   
   //Formik para validar
@@ -31,20 +38,72 @@ const crearReservasPrueba = () => {
       const fechaFormateada = format(values.FechaReserva, "dd/MM/yyyy", {
         locale: es,
       })
-      console.log(fechaFormateada);
+      const horaFormateada = format(values.HoraReserva, "HH:mm",{
+        locale: es
+      })
+      console.log(values);
+      console.log("La fecha formateada es",horaFormateada);
     },
   });
+
+  const isWeekday = (date) => {
+    const day = getDay(date);
+    return day !== 0;
+  };
+
+  const filterMinDay = () => {
+    const nextDay = new Date();
+    nextDay.setDate(date.getDate() + 1);
+    return nextDay;
+  };
+  const filterMaxDay = () => {
+    const limitDate = new Date();
+    limitDate.setMonth(date.getMonth() + 1);
+    return limitDate;
+  };
+
+  let handleColor = (time) => {
+    return time.getHours() > 11 ? "" : "d-none";
+  };
+
+
 
   return (
     <>
       <Form onSubmit={formik.handleSubmit} noValidate>
         {/*Datepicker sensillo, no funciona con id ni formik.getfieldprops, porque parece que formik no acepta valores tipo fecha en ese formato*/ }
       <DatePicker
-        selected={formik.values.FechaReserva}
-        onChange={(date) => formik.setFieldValue("FechaReserva", date)}
+      selected={formik.values.FechaReserva}
+      onChange={(date) => formik.setFieldValue("FechaReserva", date)}
+      closeOnScroll={true}
+        locale={es}
+        dateFormat="dd/MM/yyyy"
+        minDate={filterMinDay()}
+        maxDate={filterMaxDay()}
+
+        filterDate={isWeekday}
+      placeholderText="Selecciona una fecha"  
       />
       {formik.errors.FechaReserva && formik.touched.FechaReserva ? (
         <div>{formik.errors.FechaReserva}</div>
+      ) : null}
+
+<DatePicker
+         selected={formik.values.HoraReserva}
+         onChange={(hora) => formik.setFieldValue("HoraReserva", hora)}
+         showTimeSelect
+         showTimeSelectOnly
+         timeIntervals={60}
+         timeCaption="Hora"
+         dateFormat="HH:mm"
+         locale={es}
+         timeClassName={handleColor}
+         //filterTime={filterTime}
+         //value={values.time}
+         placeholderText="Selecciona una hora"
+        />
+ {formik.errors.HoraReserva && formik.touched.HoraReserva ? (
+        <div>{formik.errors.HoraReserva}</div>
       ) : null}
       <Button type="submit">Enviar</Button>
     </Form>
