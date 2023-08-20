24586c1e6a9029447d242000ea5a7fc9092d7b37
Yudi454 <lucasyudi445@gmail.com>
2023-08-20 15:14:11 -0300

    Anteriormente habia terminado lo que seria registro no login, ahora si termine lo que seria registro, todo tiene validacion y todo ya esta responsive solo falta limpiar el codigo, el registro esta conectado al db.json, mientras que el login a una base de datos de prueba para recibir el token

--- src/pages/Login/InicioSesion.jsx
+++ src/pages/Login/InicioSesion.jsx
@@ -1,52 +1,158 @@
 import { useState } from "react";
-import "../../styles/InicioSesion.css"
-import logo from '../../assets/logo.png';
+import "../../styles/InicioSesion.css";
+import logo from "../../assets/logo.png";
 import { useAuth } from "../../auth/AuthProvider";
-import { Navigate } from "react-router-dom";
-import { Form } from "react-bootstrap";
-import {useFormik} from "formik";
-import * as Yup from "yup" ;
+import { Link, Navigate } from "react-router-dom";
+import { Container, Form } from "react-bootstrap";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import clsx from "clsx";
-import Swal from 'sweetalert2';
-
+import Swal from "sweetalert2";
+import axios from "axios";
 
 function InicioSesion() {
-     
-    const auth = useAuth
+  //Url de un back de prueba para que la funcion de logueo quede guardada para cuando usemos el back
+  const url = "http://localhost:8001/api/usuarios/login";
 
-    if(auth.isAuthenticated){
-        return <Navigate to="/dashboard" />
-    }
+  //UseState para mostrar un mensaje de que los datos ingresados no se encontraron
+  const [UsuarioLogueadoError, setUsuarioLogueadoError] = useState(false);
+
+  //Esquema de Yup
+  const esquemaInicioSesion = Yup.object().shape({
+    Email: Yup.string().required("El email es requerido"),
+
+    Contraseña: Yup.string().required("La contraseña es requerida"),
+  });
+
+  //Valores Iniciales
+  const valoresIniciales = {
+    Email: "",
+    Contraseña: "",
+  };
+
+  //Validacion con formik
+  const formik = useFormik({
+    initialValues: valoresIniciales,
+    validationSchema: esquemaInicioSesion,
+    validateOnChange: true,
+    validateOnBlur: true,
+    onSubmit: async (values) => {
+      try {
+        //Guardo los valores de los inputs
+        const usuarioLogueado = {
+          email: values.Email,
+          contraseña: values.Contraseña,
+        };
+
+        //Hago el pedido con axios
+        const response = await axios.post(url, usuarioLogueado);
+
+        //Si se encuentra sale la alerta y guarda el token en LocalStorage
+        if (response.status === 200) {
+          Swal.fire(
+            "Logueado con exito",
+            "Los datos ingresaron coincidieron con su usuario exitosamente",
+            "success"
+          );
+
+          const jwtToken = response.data.data.token;
+          localStorage.setItem("user", JSON.stringify(jwtToken));
+          setUsuarioLogueadoError(false);
+        }
+      } catch (error) {
+        //Si no te muestra el mensaje de error
+        setUsuarioLogueadoError(true);
+        console.log(error);
+      }
+    },
+  });
+
+  //Nose para que sirve xd
+  const auth = useAuth;
+
+  if (auth.isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
 
   return (
-<div className="background-image">
-             
-           <div className="container">
-               <div className="Carta">
-                   <div className="center text-center">
-                       <h3 className="">Bienvenido!</h3> 
-                       <img src={logo} alt="Logo de la pagina" className="logo" />  
-                          <Form className="form">
-                            <Form.Group className="contenedorForm">
-                                <Form.Label>Ingresa tu correo electronico :</Form.Label>
-                                <Form.Control  />
-                            </Form.Group>
-                            <Form.Group className="contenedorForm">
-                                <Form.Label>Ingresa tu contraseña :</Form.Label>
-                                <Form.Control  />
-                            </Form.Group>     
-                            
-                            <button className="btn mt-3" type="submit">Ingresar</button>
-                                                   
-
-                            <p className="mb-0 mt-4 text-center"><a href="#" className="link">
-                            Olvidaste tu contraseña?</a></p>
-                               
-                           </Form> 
-
-                        </div>
-                    </div>
+    <div className="background-image">
+      <Container className="ubicarCarta">
+        <div className="Carta mt-3 mb-3 text-center">
+          <h3 className="mt-3">Bienvenido!</h3>
+          <img src={logo} alt="Logo de la pagina" className="img-fluid" />
+          {UsuarioLogueadoError === true && (
+            <div className="d-flex justify-content-center">
+              <span role="alert" className="text-danger">
+                Los datos ingresadon no coinciden con ningun usuario
+              </span>
+            </div>
+          )}
+          <Form onSubmit={formik.handleSubmit} noValidate>
+            <Form.Group className="contenedorForm">
+              <Form.Label>Ingresa tu correo electronico :</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ej: Lucas@gmail.com"
+                id="Email"
+                {...formik.getFieldProps("Email")}
+                className={clsx(
+                  "form-control",
+                  {
+                    "is-invalid": formik.touched.Email && formik.errors.Email,
+                  },
+                  {
+                    "is-valud": formik.touched.Email && !formik.errors.Email,
+                  }
+                )}
+              />
+              {formik.touched.Email && formik.errors.Email && (
+                <div>
+                  <span role="alert" className="text-danger">
+                    {formik.errors.Email}
+                  </span>
                 </div>
+              )}
+            </Form.Group>
+            <Form.Group className="contenedorForm">
+              <Form.Label>Ingresa tu contraseña :</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Ej: Lucas1234"
+                id="Contraseña"
+                {...formik.getFieldProps("Contraseña")}
+                className={clsx(
+                  "form-control",
+                  {
+                    "is-invalid":
+                      formik.touched.Contraseña && formik.errors.Contraseña,
+                  },
+                  {
+                    "is-valid":
+                      formik.touched.Contraseña && formik.errors.Contraseña,
+                  }
+                )}
+              />
+              {formik.touched.Contraseña && formik.errors.Contraseña && (
+                <div>
+                  <span role="alert" className="text-danger">
+                    {formik.errors.Contraseña}
+                  </span>
+                </div>
+              )}
+            </Form.Group>
+
+            <button className="btn mt-3 mb-3" type="submit">
+              Ingresar
+            </button>
+
+            <div className="mb-3 text-center">
+              <Link to={"/Registro"} className="link">
+                Olvidaste tu contraseña?
+              </Link>
+            </div>
+          </Form>
+        </div>
+      </Container>
     </div>
   );
 }
--- src/pages/Registro/Registro.jsx
+++ src/pages/Registro/Registro.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import "../../styles/InicioSesion.css";
 import logo from "../../assets/logo.png";
 import { useAuth } from "../../auth/AuthProvider";
-import { Navigate } from "react-router-dom";
-import { Form } from "react-bootstrap";
+import { Link, Navigate } from "react-router-dom";
+import { Container, Form } from "react-bootstrap";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import clsx from "clsx";
@@ -11,8 +11,7 @@ import Swal from "sweetalert2";
 import axios from "axios";
 
 function Registro() {
-
-    const url = import.meta.env.VITE_API_USUARIOS
+  const url = import.meta.env.VITE_API_USUARIOS;
 
   //Expresiones para validar
   const soloLetras = /^[a-zA-Z ]+$/;
@@ -86,17 +85,17 @@ function Registro() {
               Contraseña: values.Contraseña,
             };
 
-            axios.post(url,Usuario)
-            .then(response => {
+            axios
+              .post(url, Usuario)
+              .then((response) => {
                 console.log("Usuario creado con exito");
-            })
-            .catch(error => {
-                console.error("Error al cargar el usuario")
-            })
-
+              })
+              .catch((error) => {
+                console.error("Error al cargar el usuario");
+              });
 
             console.log(Usuario);
-            //Funcion para redirigirte a inicio
+            //Agregar funcion para redirigirte a inicio
           }
         });
       } catch (error) {
@@ -113,140 +112,136 @@ function Registro() {
 
   return (
     <div className="background-image">
-      <div className="container mt-4 mb-4">
-        <div className="Carta">
-          <div className="center text-center">
-            <h3 className="mt-3">No tienes cuenta?</h3>
-            <h3 className="">Create una!</h3>
-            <img src={logo} alt="Logo de la pagina" className="img-fluid" />
-            <div className="section text-center">
-              <Form onSubmit={formik.handleSubmit} noValidate>
-                <Form.Group className="contenedorForm">
-                  <Form.Label>Nombre :</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Ej: Lucas"
-                    id="Nombre"
-                    {...formik.getFieldProps("Nombre")}
-                    className={clsx(
-                      "form-control",
-                      {
-                        "is-invalid":
-                          formik.touched.Nombre && formik.errors.Nombre,
-                      },
-                      {
-                        "is-valid":
-                          formik.touched.Nombre && !formik.errors.Nombre,
-                      }
-                    )}
-                  />
-                  {formik.touched.Nombre && formik.errors.Nombre && (
-                    <div>
-                      <span role="alert" className="text-danger">
-                        {formik.errors.Nombre}
-                      </span>
-                    </div>
+      <Container className="ubicarCarta mt-4 mb-4">
+        <div className="Carta text-center">
+          <h3 className="mt-3">No tienes cuenta?</h3>
+          <h3 className="">Create una!</h3>
+          <img src={logo} alt="Logo de la pagina" className="img-fluid" />
+          <div className="section text-center">
+            <Form onSubmit={formik.handleSubmit} noValidate>
+              <Form.Group className="contenedorForm">
+                <Form.Label>Nombre :</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ej: Lucas"
+                  id="Nombre"
+                  {...formik.getFieldProps("Nombre")}
+                  className={clsx(
+                    "form-control",
+                    {
+                      "is-invalid":
+                        formik.touched.Nombre && formik.errors.Nombre,
+                    },
+                    {
+                      "is-valid":
+                        formik.touched.Nombre && !formik.errors.Nombre,
+                    }
                   )}
-                </Form.Group>
-                <Form.Group className="contenedorForm">
-                  <Form.Label>Correo Electrónico </Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Ej: lucas@gmail.com"
-                    id="Email"
-                    {...formik.getFieldProps("Email")}
-                    className={clsx(
-                      "form-control",
-                      {
-                        "is-invalid":
-                          formik.touched.Email && formik.errors.Email,
-                      },
-                      {
-                        "is-valid":
-                          formik.touched.Email && !formik.errors.Email,
-                      }
-                    )}
-                  />
-                  {formik.touched.Email && formik.errors.Email && (
-                    <div>
-                      <span role="alert" className="text-danger">
-                        {formik.errors.Email}
-                      </span>
-                    </div>
+                />
+                {formik.touched.Nombre && formik.errors.Nombre && (
+                  <div>
+                    <span role="alert" className="text-danger">
+                      {formik.errors.Nombre}
+                    </span>
+                  </div>
+                )}
+              </Form.Group>
+              <Form.Group className="contenedorForm">
+                <Form.Label>Correo Electrónico </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ej: lucas@gmail.com"
+                  id="Email"
+                  {...formik.getFieldProps("Email")}
+                  className={clsx(
+                    "form-control",
+                    {
+                      "is-invalid": formik.touched.Email && formik.errors.Email,
+                    },
+                    {
+                      "is-valid": formik.touched.Email && !formik.errors.Email,
+                    }
                   )}
-                </Form.Group>
-                <Form.Group className="contenedorForm">
-                  <Form.Label>Contrasña </Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Ej: Lucas1234"
-                    id="Contraseña"
-                    {...formik.getFieldProps("Contraseña")}
-                    className={clsx(
-                      "form-control",
-                      {
-                        "is-invalid":
-                          formik.touched.Contraseña && formik.errors.Contraseña,
-                      },
-                      {
-                        "is-valid":
-                          formik.touched.Contraseña && formik.errors.Contraseña,
-                      }
-                    )}
-                  />
-                  {formik.touched.Contraseña && formik.errors.Contraseña && (
+                />
+                {formik.touched.Email && formik.errors.Email && (
+                  <div>
+                    <span role="alert" className="text-danger">
+                      {formik.errors.Email}
+                    </span>
+                  </div>
+                )}
+              </Form.Group>
+              <Form.Group className="contenedorForm">
+                <Form.Label>Contrasña </Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Ej: Lucas1234"
+                  id="Contraseña"
+                  {...formik.getFieldProps("Contraseña")}
+                  className={clsx(
+                    "form-control",
+                    {
+                      "is-invalid":
+                        formik.touched.Contraseña && formik.errors.Contraseña,
+                    },
+                    {
+                      "is-valid":
+                        formik.touched.Contraseña && formik.errors.Contraseña,
+                    }
+                  )}
+                />
+                {formik.touched.Contraseña && formik.errors.Contraseña && (
+                  <div className="Div-Contraseña">
+                    <span role="alert" className="text-danger">
+                      {formik.errors.Contraseña}
+                    </span>
+                  </div>
+                )}
+              </Form.Group>
+              <Form.Group className="contenedorForm">
+                <Form.Label>Repite tu contraseña </Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Ingresa nuevamente la contraseña"
+                  id="ConfirmarContraseña"
+                  {...formik.getFieldProps("ConfirmarContraseña")}
+                  className={clsx(
+                    "form-control",
+                    {
+                      "is-invalid":
+                        formik.touched.ConfirmarContraseña &&
+                        formik.errors.ConfirmarContraseña,
+                    },
+                    {
+                      "is-valid":
+                        formik.touched.ConfirmarContraseña &&
+                        !formik.errors.ConfirmarContraseña,
+                    }
+                  )}
+                />
+                {formik.touched.ConfirmarContraseña &&
+                  formik.errors.ConfirmarContraseña && (
                     <div className="Div-Contraseña">
                       <span role="alert" className="text-danger">
-                        {formik.errors.Contraseña}
+                        {formik.errors.ConfirmarContraseña}
                       </span>
                     </div>
                   )}
-                </Form.Group>
-                <Form.Group className="contenedorForm">
-                  <Form.Label>Repite tu contraseña </Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Ingresa nuevamente la contraseña"
-                    id="ConfirmarContraseña"
-                    {...formik.getFieldProps("ConfirmarContraseña")}
-                    className={clsx(
-                      "form-control",
-                      {
-                        "is-invalid":
-                          formik.touched.ConfirmarContraseña &&
-                          formik.errors.ConfirmarContraseña,
-                      },
-                      {
-                        "is-valid":
-                          formik.touched.ConfirmarContraseña &&
-                          !formik.errors.ConfirmarContraseña,
-                      }
-                    )}
-                  />
-                  {formik.touched.ConfirmarContraseña &&
-                    formik.errors.ConfirmarContraseña && (
-                      <div className="Div-Contraseña">
-                        <span role="alert" className="text-danger">
-                          {formik.errors.ConfirmarContraseña}
-                        </span>
-                      </div>
-                    )}
-                </Form.Group>
+              </Form.Group>
 
-                <button className="btn mt-3 mb-3" type="submit">
-                  Crear Cuenta
-                </button>
+              <button className="btn mt-3 mb-3" type="submit">
+                Crear Cuenta
+              </button>
 
-                <p className="mb-0 mt-4 text-center mb-2">
-                  <a href="#" className="link ">
-                    Ya tienes una cuenta? Inicia Sesion?
-                  </a>
-                </p>
-              </Form>
-            </div>
+              <div className=" text-center mb-3">
+                <Link to={"/Login"} className="link ">
+                  Ya tienes una cuenta? Inicia Sesion?
+                </Link>
+              </div>
+            </Form>
           </div>
         </div>
-      </div>
+      </Container>
     </div>
   );
 }
