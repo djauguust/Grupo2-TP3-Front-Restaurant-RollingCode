467cd3300298ca7c2704b1576f3998e9c102f416
Mauro <mauro10z@hotmail.com>
2023-08-20 12:21:47 -0300

    Correggi importacion de css

--- src/pages/a-cerca-de-nosotros/ACercaDeNosotros.jsx
+++ src/pages/a-cerca-de-nosotros/ACercaDeNosotros.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
