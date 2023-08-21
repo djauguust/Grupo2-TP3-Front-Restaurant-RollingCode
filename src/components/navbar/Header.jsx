72f9f7ddd4777e201ac78594b3b6a6a484450a8d
PedroAntich <pedroantichi@gmail.com>
2023-08-21 02:07:37 -0300

    cambio boton switch

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -103,8 +103,8 @@ const Header = () => {
           >
             {user ? (
               <>
-                <Button
-                  className=" boton-izq-custom"
+                <Nav.Link
+                  className=" boton-perfil-izq-custom"
                   as={Link}
                   onClick={handleShow}
                   variant="dark"
@@ -121,7 +121,7 @@ const Header = () => {
                     <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
                   </svg>
                   Ver perfil
-                </Button>
+                </Nav.Link>
                 <Offcanvas show={show} onHide={handleClose} placement="end">
                   <Offcanvas.Header closeButton>
                     <Offcanvas.Title>
@@ -217,8 +217,8 @@ const Header = () => {
               </svg>{" "}
               {t('ubicacion')}
             </Nav.Link>
-            <Dropdown>
-              <Dropdown.Toggle className="boton-izq-custom" variant="primary-outline"  id="dropdown-basic">
+            <Dropdown className="drop-idioma">
+              <Dropdown.Toggle className="boton-izq-custom-idioma" variant="none"  id="dropdown-basic">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -232,21 +232,22 @@ const Header = () => {
                 {t('idioma')}
               </Dropdown.Toggle>
 
-              <Dropdown.Menu>
+              <Dropdown.Menu >
                 <Dropdown.Item  onClick={() => i18n.changeLanguage('es')}>Español</Dropdown.Item>
                 <Dropdown.Item onClick={() => i18n.changeLanguage('en')}>Ingles</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </Nav>
+          <Form.Check
+  type="switch"
+  id="custom-switch"
+  label=""
+  onChange={handleSwitch}
+  className="boton-switch"
+  variant="dark"
+  custom // Agregar esta clase para habilitar los estilos personalizados
+/>
         </Navbar.Collapse>
-        <Form>
-      <Form.Check // prettier-ignore
-        type="switch"
-        id="custom-switch"
-        label=""
-        onChange={handleSwitch}
-      />
-    </Form>
       </Navbar>
     </>
   );
