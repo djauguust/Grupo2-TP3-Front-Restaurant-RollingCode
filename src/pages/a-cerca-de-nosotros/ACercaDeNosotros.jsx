56d9de8e980681d118d60a37eee73eac761608ef
Mauro <mauro10z@hotmail.com>
2023-08-23 19:31:44 -0300

    Modificando descripcion de equipo

--- src/pages/a-cerca-de-nosotros/ACercaDeNosotros.jsx
+++ src/pages/a-cerca-de-nosotros/ACercaDeNosotros.jsx
@@ -11,7 +11,7 @@ const ACercaDeNosotros = () => {
   return (
     <>
       <h1 className="about-title">A Cerca De Nosotros</h1>
-      <p className="text-center">Somos un grupo de programadores que...</p>
+      <p className="text-center px-5">Somos un equipo de desarrolladores comprometidos en la creación de un proyecto innovador para RollingCode. Nuestro grupo está formado por mentes creativas y dedicadas. Aprovechamos esta gran oportunidad que nos brinda la tecnologia para diseñar soluciones que aborden las necesidades únicas del instituto y aportar nuestro valor. Trabajamos en colaboración, combinando nuestras habilidades en desarrollo de software, diseño de experiencia de usuario y gestión de proyectos. Nuestra visión es brindar nuestra humilde experiencia para cumplir con un sistema de gestion para un restaurante, comprometidos a hacer realidad esa visión en cada línea de código que escribimos y en cada función que diseñamos.</p>
 
       <Container>
         <Row>
