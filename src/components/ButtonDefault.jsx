3c6457edaa1c043565c3f39a1a7e1147d2dfec88
Lucas Ybanez <luck.facebook@hotmail.com>
2023-08-24 17:42:00 -0300

    botón registrar en login

--- src/components/ButtonDefault.jsx
+++ src/components/ButtonDefault.jsx
@@ -7,7 +7,7 @@ function ButtonDefault({ namebtn, TipoBoton, funcion, to = null }) {
     <>
       {to ?
        ( <Link to={"/Registro"} className="lin0k" >
-          Olvidaste tu contraseña?
+          ¿No tienes cuenta? ¡Crea una!
 
         </Link>):(
          <button className="btn mt-3 mb-3" type={TipoBoton} >{namebtn}</button>
